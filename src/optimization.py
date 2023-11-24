297d64d3207afd20aaeb29f0164e495d0d5ed9aa
JULIA EVERITT <juliaeveritt13@gmail.com>
2023-11-24 14:47:09 -0800

    debug tests

--- src/optimization.py
+++ src/optimization.py
@@ -1,11 +1,13 @@
-import pandas as pd
+from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 
-def optimize_hyperparameter(X_train, y_train, pipe, param_grid, njobs=-1, cv=5,method=GridSearchCV):
+
+def optimize_hyperparameter(X_train, y_train, pipe, 
+                            param_grid, njobs=-1, cv=5, method=GridSearchCV):
     """
     Optimize the hyperparameter(s) of logistic regression model.
 
-    Uses exhaustive grid search to optimize the hyperparameters of a 
+    Uses exhaustive grid search to optimize the hyperparameters of a
     linear model using 5 fold cross validation by default.
 
     Parameters:
@@ -17,8 +19,8 @@ def optimize_hyperparameter(X_train, y_train, pipe, param_grid, njobs=-1, cv=5,m
     pipe : scikit-learn Pipeline object
         The pipeline containing a list of the estimator objects.
     param_grid : numpy.dict
-        A numpy dictionary containing the hyperparameter(s) to optimize and the range to
-        optimize on.
+        A numpy dictionary containing the hyperparameter(s) 
+        to optimize and the range to optimize on.
     njobs : int
         (optional, default=-1) The number of jobs to run in parallel.
     cv : int
@@ -37,19 +39,25 @@ def optimize_hyperparameter(X_train, y_train, pipe, param_grid, njobs=-1, cv=5,m
     >>> from sklearn.model_selection import GridSearchCV
     >>> from sklearn.pipeline import make_pipeline
     >>> from sklearn.linear_model import LogisticRegression
-    >>> param_grid = param_grid = {"logisticregression__C": np.array([0.01,0.1,1,10,100,1000])}
-    >>> pipe_lr = make_pipeline(StandardScaler(), LogisticRegression(max_iter=2000))
-    >>> X_train = pd.DataFrame({'a': [1, 2, 3],'b': [400, 500, 600],'c': [0.007, 0.008, 0.009]})
+    >>> param_grid = param_grid = {"logisticregression__C": 
+    >>>                 np.array([0.01,0.1,1,10,100,1000])}
+    >>> pipe_lr = make_pipeline(StandardScaler(), 
+    >>>                 LogisticRegression(max_iter=2000))
+    >>> X_train = pd.DataFrame({'a': [1, 2, 3],
+    >>>                 'b': [400, 500, 600],
+    >>>                 'c': [0.007, 0.008, 0.009]})
     >>> y_train = pd.DataFrame({'Label': ['class1', 'class2', 'class1']})
-    >>> X_test = pd.DataFrame({'a': [4, 5],'b': [700, 800],'c': [0.001, 0.002],})
+    >>> X_test = pd.DataFrame({'a': [4, 5],
+    >>>                 'b': [700, 800],
+    >>>                 'c': [0.001, 0.002],})
     >>> y_test = pd.DataFrame({'Label': ['class2', 'class1']})
     >>> results = optimize_hyperparameter(X_train, y_train, pipe, param_grid)
     
     """
-    if (type(pipe) != sklearn.pipeline.Pipeline):
+    if (not isinstance(pipe, Pipeline)):
         raise TypeError("pipe must be type sklearn.pipeline.Pipeline")
     
-    if (type(param_grid) != dict):
+    if (not isinstance(param_grid, dict)):
         raise TypeError("param_grid must be type dictionary")
 
     if (not param_grid):
--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -5,7 +5,7 @@ import sys
 import os
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.linear_model import LogisticRegression
 
 # Import the optimization function
@@ -22,11 +22,13 @@ pipe_lr = make_pipeline(
 )
 
 X_train = pd.DataFrame({
-        'a': [1, 2, 3],
-        'b': [400, 500, 600],
-        'c': [0.007, 0.008, 0.009]
+        'a': [1, 2, 3, 4, 5, 6, 7, 8],
+        'b': [100, 200, 300, 400, 500, 600, 700, 800],
+        'c': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
     })
-y_train = pd.DataFrame({'Label': ['class1', 'class2', 'class1']})
+y_train = pd.DataFrame({'Label': ['class1', 'class2', 'class1', 
+                                  'class1', 'class2', 'class1',
+                                  'class1', 'class2']})
 
 X_test = pd.DataFrame({
         'a': [4, 5],
@@ -41,7 +43,7 @@ def test_optimization_returns_gridsearchcv():
                                     y_train, 
                                     pipe_lr, 
                                     param_grid)
-    assert isinstance(output, sklearn.model_selection._search.GridSearchCV), "optimization should return a GridSearchCV object"
+    assert isinstance(output, GridSearchCV), "optimization should return a GridSearchCV object"
 
 # Test hyperparameters have been optimized
 def test_optimization_successful():
@@ -50,7 +52,7 @@ def test_optimization_successful():
                                     pipe_lr, 
                                     param_grid).best_params_
     assert isinstance(output, dict), "optimal hyperparameters should be accessible in a dictionary object"
-    assert isinstance(bool(output), True), "optimal hyperparameter dict should not be empty"
+    assert len(output) != 0, "optimal hyperparameter dict should not be empty"
 
 # Test cross validation results are available and of expected type
 def test_optimization_cv_available():
@@ -58,7 +60,7 @@ def test_optimization_cv_available():
                                     y_train, 
                                     pipe_lr, 
                                     param_grid).cv_results_
-    assert isinstance(output['mean_train_score'], np.array), "cv results should be available as array with cv_results_ attribute"
-    assert isinstance(output['mean_test_score'], np.array), "cv results should be available as array with cv_results_ attribute"
+    assert isinstance(output['mean_train_score'], np.ndarray), "cv results should be available as array with cv_results_ attribute"
+    assert isinstance(output['mean_test_score'], np.ndarray), "cv results should be available as array with cv_results_ attribute"
     assert len(output['mean_train_score']) != 0, "cv train results should not be empty"
     assert len(output['mean_test_score']) != 0, "cv test results should not be empty"
