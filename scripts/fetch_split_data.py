47b39e7eae1f95e2839f8a8b157bbbadf38ea82c
HinaBandukwala <hnbandukwala@gmail.com>
2023-11-28 15:37:33 -0800

    modified fetch_split_data script

--- scripts/fetch_split_data.py
+++ scripts/fetch_split_data.py
@@ -2,18 +2,23 @@ import pandas as pd
 import os
 import click
 from sklearn.model_selection import train_test_split
+from ucimlrepo import fetch_ucirepo
 
 @click.command()
 @click.option('--output_path')
-def main(output_path):
+@click.option('--ucirepo_id', type=int)
+def main(output_path, id):
     # fetch dataset 
-    wine = fetch_ucirepo(id=109) 
+    wine = fetch_ucirepo(id=id) 
 
     #Split into train/test with equal distribution of target classes
-    wine_train, wine_test = train_test_split(
+    train, test = train_test_split(
         wine.data.original, train_size=0.70, stratify=wine.data.original['class']
     )
 
     #Save split data
-    wine_train.to_csv(os.path.join(output_path, f"{wine_train}"))
-    wine_test.to_csv(os.path.join(output_path, f"{wine_test}"))
+    train.to_csv(os.path.join(output_path, "train.csv"))
+    test.to_csv(os.path.join(output_path,  "test.csv"))
+
+if __name__ in '__main__':
+    main()
