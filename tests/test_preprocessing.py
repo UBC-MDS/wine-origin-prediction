bce938f553c56831d4c69d1ca596809db31125f5
YimengXia <sara.xia9105@gmail.com>
2023-12-08 06:54:55 -0800

    another expected input scenario added

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -9,18 +9,18 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from src.function_preprocessing import preprocessing
 
-# Test data
+# Test data scenario 1
 train_data = pd.DataFrame({
-        'a': [1, 2, 3],
-        'b': [400, 500, 600],
-        'c': [0.007, 0.008, 0.009],
+        'a': [1, 5, 9],
+        'b': [200, 600, 100],
+        'c': [0.009, 0.008, 0.008],
         'Label': ['class1', 'class2', 'class1']
     })
 
 test_data = pd.DataFrame({
-        'a': [4, 5],
-        'b': [700, 800],
-        'c': [0.001, 0.002],
+        'a': [3, 7],
+        'b': [400, 800],
+        'c': [0.002, 0.006],
         'Label': ['class2', 'class1']
     })
     
@@ -37,11 +37,39 @@ all_cols = numerical_cols + categorical_cols
 output_train_path = "./scaled_wine_train.csv"
 output_test_path = "./scaled_wine_test.csv"
 
+# Test data scenario 2
+extra_train_data = pd.DataFrame({
+        'x': [10, 20, 28],
+        'y': ['high', 'medium', 'low'],
+        'z': [0.02, 0.025, 0.03],
+        'Target': ['alpha', 'beta', 'gamma']
+    })
+
+extra_test_data = pd.DataFrame({
+        'x': [12, 25],
+        'y': ['medium', 'high'],
+        'z': [0.015, 0.018],
+        'Target': ['beta', 'alpha']
+    })
+
+extra_variables = pd.DataFrame({
+        'name': ['x', 'y', 'z', 'Target'],
+        'role': ['Feature', 'Feature', 'Feature', 'Target'],
+        'type': ['Integer', 'Categorical', 'Continuous', 'Categorical']
+    })
+
+extra_numerical_cols = extra_variables[extra_variables['type'].isin(['Integer', 'Continuous'])]['name'].tolist()
+extra_categorical_cols = ['y', 'Target']
+extra_all_cols = extra_numerical_cols + extra_categorical_cols
+
+extra_output_train_path = "./scaled_data_train_expected.csv"
+extra_output_test_path = "./scaled_data_test_expected.csv"
+
 # Test for correct return type
 def test_preprocessing_returns_df():
-    preprocessor = preprocessing(train_data, test_data, output_train_path, output_test_path, numerical_cols)
-    output_train = pd.DataFrame(preprocessor.transform(train_data), columns=all_cols)
-    output_test = pd.DataFrame(preprocessor.transform(test_data), columns=all_cols)
+    preprocessor = preprocessing(extra_train_data, extra_test_data, extra_output_train_path, extra_output_test_path, extra_numerical_cols)
+    output_train = pd.DataFrame(preprocessor.transform(extra_train_data), columns=extra_all_cols)
+    output_test = pd.DataFrame(preprocessor.transform(extra_test_data), columns=extra_all_cols)
     assert isinstance(output_train, pd.DataFrame)
     assert isinstance(output_test, pd.DataFrame)
 
